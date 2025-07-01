2c1e67106652464121d0bace62ce703d03c3cad8
鬼影233 <82418456+gui-ying233@users.noreply.github.com>
2025-06-30 20:55:50 -0400

    example

--- upload.example.js
+++ upload.example.js
@@ -1,16 +1,15 @@
 const { mw } = require("./mediaWiki");
 const api = new mw.Api(require("./config").cm);
 const { createReadStream, statSync } = require("fs");
+
 (async () => {
 	const filepath = "example.jpg";
-	const filesize = statSync(filepath).size;
-	const file = createReadStream(filepath, { highWaterMark: 1000000 });
 	await api.login();
 	const r = await api.post({
 		action: "upload",
 		filename: "example",
-		file,
-		filesize,
+		file: createReadStream(filepath, { highWaterMark: 1000000 }),
+		filesize: statSync(filepath).size,
 		token: await api.getToken("csrf"),
 		comment: "upload TEST",
 		text: "TEST",
