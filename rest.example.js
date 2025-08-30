f6300d71b7d0c8d0b35e27ef3ebe09dd35976f95
鬼影233 <82418456+gui-ying233@users.noreply.github.com>
2025-08-30 12:16:25 -0400

    REST example

---
+++ rest.example.js
@@ -0,0 +1,10 @@
+const { mw } = require("./mediaWiki");
+const api = new mw.Api(require("./config").mzh);
+const rest = new mw.Rest(
+	Object.assign(require("./config").mzh, {
+		url: "https://mzh.moegirl.org.cn/rest.php/v1/",
+	})
+);
+(async () => {
+	console.log(await rest.get("page/H:沙盒/bare"));
+})();
