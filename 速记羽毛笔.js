f2a9afd895ae139f13d8bc4174c12adf581a0b64
鬼影233 <82418456+gui-ying233@users.noreply.github.com>
2025-10-28 13:00:16 -0400

    更新

--- 速记羽毛笔.js
+++ 速记羽毛笔.js
@@ -1,38 +1,45 @@
 "use strict";
 
-const { MediaWikiJS } = require("@lavgup/mediawiki.js");
-const bot = new MediaWikiJS(require("./config.json").mzh);
-bot.login().then(async () => {
-	try {
-		const result0 = await bot.api.get({
-			action: "query",
-			prop: "links",
-			titles: "User talk:鬼影233/唱唱反调",
-			pllimit: "max",
-		});
-		console.log(`共${result0.query.pages[0].links.length}个页面。`);
-		for (let i = 0; i < result0.query.pages[0].links.length; i++) {
-			try {
-				console.log(
-					`第${i + 1}个页面：${
-						result0.query.pages[0].links[i].title
-					}。`
-				);
-				const result1 = await bot.doEdit({
-					title: result0.query.pages[0].links[i].title,
-					appendtext:
-						'\n\n{{safesubst:U:鬼影233/唱唱反调}}<span style="display:none;">{{mute|{{safesubst:ROOTPAGENAME}}}} 四月是你的谎言~~~</span>',
-					summary:
-						"速记羽毛笔：新一期的《唱唱反调》已送达，请注意查收~",
-					tags: "Bot",
-					Bot: true,
-				});
-				console.log(result1.edit);
-			} catch (e1) {
-				console.error(e1);
+const { mw } = require("./mediaWiki");
+const api = new mw.Api(require("./config").mzh);
+
+(async () => {
+	await api.login();
+	const pages = await api.post({
+		action: "query",
+		prop: "links",
+		titles: "User talk:鬼影233/唱唱反调",
+		plnamespace: "3|2",
+		pllimit: "max",
+	});
+	console.log(`共${pages.query.pages[0].links.length}个页面。`);
+	const edit = async title => {
+		try {
+			const r = await api.post({
+				action: "edit",
+				title,
+				appendtext:
+					'\n\n{{safesubst:U:鬼影233/唱唱反调}}<span style="display:none;">{{mute|{{safesubst:ROOTPAGENAME}}}} 《关于处理萌娘百科管理层向编辑滥用职权行为的公告》~~~</span>',
+				summary: "速记羽毛笔：新一期的《唱唱反调》已送达，请注意查收~",
+				tags: "Bot",
+				bot: true,
+				nocreate: true,
+				token: await api.getToken("csrf"),
+			});
+			if (r?.error?.code === "badtoken") {
+				console.warn("badtoken");
+				await api.getToken("csrf", true);
+				return await edit(title);
 			}
+			console.log(r.edit);
+		} catch (e) {
+			console.error(e);
 		}
-	} catch (e0) {
-		console.error(e0);
+	};
+	for (let i = 1; i < pages.query.pages[0].links.length; i++) {
+		console.log(
+			`第${i + 1}个页面：${pages.query.pages[0].links[i].title}。`
+		);
+		await edit(pages.query.pages[0].links[i].title);
 	}
-});
+})();
