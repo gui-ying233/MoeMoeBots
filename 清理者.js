8fbb3c2fbb03c1e6e6e6e936424a9b0f25ea3b70
鬼影233 <82418456+gui-ying233@users.noreply.github.com>
2026-02-17 03:02:27 -0500

    Unrecognized values for parameter "gcmnamespace": -2, -1, 2300, 2302, 2303

--- 清理者.js
+++ 清理者.js
@@ -45,7 +45,7 @@ const {
 										rvprop: "content|timestamp",
 										generator: "categorymembers",
 										gcmnamespace:
-											"-2|-1|0|1|4|5|6|7|8|9|10|11|12|13|14|15|274|275|710|711|828|829|2300|2302|2303",
+											"0|1|4|5|6|7|8|9|10|11|12|13|14|15|274|275|710|711|828|829",
 										gcmlimit: "max",
 										gcmtitle,
 									}),
