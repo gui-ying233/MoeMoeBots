1218a2d9793facaf23654e5100e4928d0bc147c7
鬼影233 <82418456+gui-ying233@users.noreply.github.com>
2026-06-04 13:19:34 -0400

    Help:沙盒 -> Draft:沙盒

--- 归零者-zh.js
+++ 归零者-zh.js
@@ -33,7 +33,7 @@ const {
 									tags: "Bot",
 									bot: true,
 									token: await api.getToken("csrf", retry),
-									title: "Help:沙盒/json",
+									title: "Draft:沙盒/json",
 									text: '{"_addText":"{{沙盒顶部}}"}',
 									summary:
 										"沙盒清理作业，若想保留较长时间，可以在[[Special:我的用户页/Sandbox.json|个人测试区]]作测试，或者翻阅历史记录。",
